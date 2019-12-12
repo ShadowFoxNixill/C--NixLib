a60c4333c712f894c5a6d5808daea63478f6bea6
ShadowFoxNixill <github@nix.nixill.net>
2019-12-12 00:07:13 -0500

    Work on CacheMap

--- src/Random/WeightedRandom.cs
+++ src/Random/WeightedRandom.cs
@@ -1,13 +1,10 @@
 using System.Collections.Generic;
 
-namespace Nixill.Random
-{
-  public class WeightedRandom<T>
-  {
+namespace Nixill.Random {
+  public class WeightedRandom<T> {
     private SortedDictionary<int, T> Backing;
 
-    public WeightedRandom()
-    {
+    public WeightedRandom() {
       Backing = new SortedDictionary<int, T>();
     }
 
---
+++ src/Utils/CacheMap.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nixill.Utils {
+  public class CacheMap<K, V> {
+    private Dictionary<K, V> Backing;
+    private Func<K, V> Builder;
+
+    public CacheMap(Func<K, V> inBuild) {
+      Builder = inBuild;
+      Backing = new Dictionary<K, V>();
+    }
+
+    public int Count { get => Backing.Count; }
+
+    public V this[K key] {
+      get => Get(key);
+      set => Set(key, value);
+    }
+
+    public V Get(K key) {
+      V ret;
+      if (Backing.TryGetValue(key, out ret)) {
+        return ret;
+      }
+      else {
+        ret = Builder.Invoke(key);
+        Backing[key] = ret;
+        return ret;
+      }
+    }
+
+    public void Set(K key, V value) {
+      Backing[key] = value;
+    }
+
+    public void Clear() {
+
+    }
+  }
+}
