9e0194a48ad12fc0ed5dbaf75c8db82cd3dbcd7e
ShadowFoxNixill <github@nix.nixill.net>
2019-12-11 19:39:03 -0500

    More work on WeightedRandom

--- src/Random/WeightedRandom.cs
+++ src/Random/WeightedRandom.cs
@@ -1,5 +1,16 @@
-namespace Nixill.Random {
-  public class WeightedRandom {
-    
+using System.Collections.Generic;
+
+namespace Nixill.Random
+{
+  public class WeightedRandom<T>
+  {
+    private SortedDictionary<int, T> Backing;
+
+    public WeightedRandom()
+    {
+      Backing = new SortedDictionary<int, T>();
+    }
+
+
   }
 }
